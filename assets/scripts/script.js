c57197fd7aa6cfd278d39540fc084c22e8ffeaf4
tyleradams2211 <tyleradams2211@gmail.com>
2022-10-02 09:45:30 -0700

    started testing js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,9 +2,11 @@ var charSymbols = document.getElementById('symbols');
 var charNumbers = document.getElementById('numbers');
 var charUpperCase = document.getElementById('upperCase');
 var charLowerCase = document.getElementById('lowerCase');
+var viewPW = document.getElementById('viewPW');
 
 var copyPW = document.getElementById('copyPW');
 
+var lengthPW = Element.getElementById('slider-range');
 
 // character collection for each char selection
 const symbols = "!@#$%^&*_-+=";
@@ -12,9 +14,27 @@ const numbers = "0123456789";
 const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lower = "abcdefghijklmnopqrstuvwxyz";
 
-document.getElementById('generatePW').addEventListener("click", event => {
-    event.preventDefault();
-    
+console.log('symbols');
+console.log('numbers');
+console.log('upper');
+console.log('lower');
 
 
-});
+// document.getElementById('generateBut').addEventListener("click", event => {
+//     event.preventDefault();  
+//     var charSelected = "";
+//         charLowerCase.checked ? (charSelected += lower) : "";
+//         charUpperCase.checked ? (charSelected += upper) : "";
+//         charNumbers.checked ? (charSelected += numbers) : "";
+//         charSymbols.checked ? (charSelected += symbols) : "";
+//         charSelected.split('');
+//     var pw = generatePW(lengthPW, charSelected);
+// });
+
+// var generatePW = (length, charSelected) => {
+//     var pw = "";
+//     for (var i = 0; i < length; i++) {
+//         pw += charSelected.charAt(Math.floor(Math.random() * charSelected.length));
+//     }
+//         return pw;
+// };
