79193e86404395ba7349a6b798c83111776467c3
tyleradams2211 <tyleradams2211@gmail.com>
2022-10-02 08:24:47 -0700

    added script file and assets folder/declared vars

---
+++ assets/scripts/script.js
@@ -0,0 +1,20 @@
+var charSymbols = document.getElementById('symbols');
+var charNumbers = document.getElementById('numbers');
+var charUpperCase = document.getElementById('upperCase');
+var charLowerCase = document.getElementById('lowerCase');
+
+var copyPW = document.getElementById('copyPW');
+
+
+// character collection for each char selection
+const symbols = "!@#$%^&*_-+=";
+const numbers = "0123456789";
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lower = "abcdefghijklmnopqrstuvwxyz";
+
+document.getElementById('generatePW').addEventListener("click", event => {
+    event.preventDefault();
+    
+
+
+});
