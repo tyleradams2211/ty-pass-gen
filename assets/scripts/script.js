813cd4a3135ffa3c2577c461d95452eb4e143ee7
tyleradams2211 <tyleradams2211@gmail.com>
2022-10-03 11:31:56 -0700

    moved scripts to assets

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,92 +1,76 @@
-var passLength = document.getElementById('lengthPW');
-var charLower = document.getElementById('lowerCase');
+var genBtn = document.getElementById('genBtn');
+var copyBtn = document.getElementById('copyBtn');
+var pwView = document.getElementById('pwView');
+var checkUpper = "";
+var checkNumbers = "";
+var checkSymbols = "";
+var checkLower = "";
+// character pools that will be selected for use and added to charTot 
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var symbols = "!@#$%^&*"
+var numbers = "0123456789"
+// charTot will be the total characters selected for use in generation
+var charTot = "";
+// Prompts and listens for responses to determine which characters are put into charTots pool.
+genBtn.addEventListener('click', event =>{
+        event.preventDefault();
+        charTot = "";
+        checkLower = prompt("Include lower case?(y/n)").charAt(0);
+            if (checkLower == "y"){
+                charTot = charTot += lower;
+            }
+        checkUpper = prompt("Include upper case?(y/n)").charAt(0);
+            if (checkUpper == 'y'){
+                charTot = charTot += upper;
+            }
+        checkNumbers = prompt("Include numbers?(y/n)").charAt(0);
+            if (checkNumbers == 'y'){
+                // numbers and symbols are added twice to increase priority on gen.
+                charTot = charTot += numbers;
+                charTot = charTot += numbers;
+            }
+        checkSymbols = prompt("Include symbols?(y/n)").charAt(0);
+            if (checkSymbols == 'y'){
+                // numbers and symbols are added twice to increase priority on gen.
+                charTot = charTot += symbols;
+                charTot = charTot += symbols;
+            }
+        var pwLength = document.getElementById('pwLength').value;
+        var pw = genPW(pwLength, charTot);
+        pwView.innerText = pw;
+    });
+// Generates the password by generating a random number and multiplying by the amount of characters selected and pulling the products location from charTot
+var genPW = function(length, charTot) {
+    pw = "";
+    for (var i = 0; i < length; i++){
+        pw += charTot.charAt(Math.floor(Math.random() * charTot.length));
+    }
+    return pw;
+}
+// Copies the pw value to the clipboard
+copyBtn.addEventListener("click", event =>{
+    var copyPW = document.getElementById('pwView');
+       //copies the text
+       navigator.clipboard.writeText(pw);
+       // alerts that the pw was copied
+       alert(pw + " was copied.");
+});
+// this code is left as prompts annoy me as a user and I want to change this back after I turn in
+// var checkUpper = document.getElementById('upper');
+// var checkNumbers = document.getElementById('numbers');
+// var checkSymbols = document.getElementById('symbols');
+// var checkLower = document.getElementById('lower')
 
-// charLower.addEventListener('change', () => {
-//     if(charLower.checked){
-//         console.log('charLower checked');
-//     }else{
-//         console.log('charLower unchecked');
-//     }
-// });
-
-
-console.log(passLength);
-console.log(charLower);
-
-document.getElementById('genBtn').addEventListener('click', function(){
-    var 
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const charSymbols = document.getElementById('symbols');
-// const charNumbers = document.getElementById('numbers');
-// const charUpperCase = document.getElementById('upperCase');
-// const charLowerCase = document.getElementById('lowerCase');
-// const viewPW = document.getElementById('viewPW');
-
-// const copyPW = document.getElementById('copyPW');
-
-// var lengthPW = document.getElementById('slider-range');
-
-
-
-
-
-
-// // character collection for each char selection
-// const symbols = "!@#$%^&*_-+=";
-// const numbers = "0123456789";
-// const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// const lower = "abcdefghijklm(nopqrstuvwxyz";
-
-// console.log(symbols);
-// console.log(numbers);
-// console.log(upper);
-// console.log(lower);
-// console.log(charLowerCase);
-// console.log();
-// console.log('lower');
-
-
-
-// document.getElementById('generateBut').addEventListener("click", event => {
-//     event.preventDefault();  
-//     var charSelected = "";
-//         charLowerCase.checked ? (charSelected += lower) : "";
-//         charUpperCase.checked ? (charSelected += upper) : "";
-//         charNumbers.checked ? (charSelected += numbers) : "";
-//         charSymbols.checked ? (charSelected += symbols) : "";
-//         charSelected.split('');
-//     var pw = generatePW(lengthPW, charSelected);
-// });
-
-// var generatePW = (length, charSelected) => {
-//     var pw = "";
-//     for (var i = 0; i < length; i++) {
-//         pw += charSelected.charAt(Math.floor(Math.random() * charSelected.length));
-//     }
-//         return pw;
-// };
+        // if (checkLower.checked){
+        //     charTot = charTot += lower;
+        // }
+        // if (checkNumbers.checked){
+        //     charTot = charTot += numbers;
+        // }
+        // if (checkSymbols.checked){
+        //     charTot = charTot += symbols;
+        // }
+        // if (checkUpper.checked) {
+        //     charTot = charTot += upper;
+        // }
