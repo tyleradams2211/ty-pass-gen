9c82095588309abc1a8ae1403128d6b39d2759d2
tyleradams2211 <tyleradams2211@gmail.com>
2022-10-02 17:08:23 -0700

    re added scripts, pw needs innertext from clog, rest of HTML/style

--- script.js
+++ script.js
@@ -1,5 +1,57 @@
-var tstBox = document.getElementById('testbox');
-var tstBtn = document.getElementById('testbutton');
+var checkUpper = document.getElementById('upper');
+var checkNumbers = document.getElementById('numbers');
+var checkSymbols = document.getElementById('symbols');
+var genBtn = document.getElementById('genBtn');
+var pwView = document.getElementById('pwView');
+
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var symbols = "!@#$%^&*"
+var numbers = "0123456789"
+
+var charTot = "";
+
+genBtn.addEventListener('click', event =>{
+        event.preventDefault();
+        charTot = lower;
+        if (checkNumbers.checked){
+            charTot = charTot += numbers;
+        }
+        if (checkSymbols.checked){
+            charTot = charTot += symbols;
+        }
+        if (checkUpper) {
+            charTot = charTot += upper;
+        }
+
+        var pwLength = document.getElementById('pwLength').value;
+         
+        var pw = genPW(pwLength, charTot);
+        console.log(pw);
+        
+    });
+
+var genPW = function(length, charTot) {
+    pw = "";
+    for (var i = 0; i < length; i++){
+        pw += charTot.charAt(Math.floor(Math.random() * charTot.length));
+    }
+    return pw;
+}
+// console.log(lower);
+
+// checkUpper.addEventListener('change', () => {
+//     if (checkUpper.checked){
+//         charTot = lower += upper;
+//         console.log(lower);
+
+//     } else {
+//         charTot = lower;
+//         console.log(lower);
+
+//     }
+// });
+// console.log(lower);
 
 // tstBox.addEventListener('change', () => {
 // if (tstBox.checked) {
@@ -11,10 +63,3 @@ var tstBtn = document.getElementById('testbutton');
 // });
 
 
-tstBtn.addEventListener('click', () =>{
-if (tstBox.checked) {
-    console.log('test is checked');
-}else {
-    console.log('test is unchecked');
-}
-});
