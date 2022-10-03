cf47eede027a2e74c1d1b6e36e12dc21c72e7587
tyleradams2211 <tyleradams2211@gmail.com>
2022-10-02 18:46:11 -0700

    added prompts

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-var checkUpper = document.getElementById('upper');
-var checkNumbers = document.getElementById('numbers');
-var checkSymbols = document.getElementById('symbols');
+        // this code is left as prompts annoy me as a user and I want to change this back after I turn in
+// var checkUpper = document.getElementById('upper');
+// var checkNumbers = document.getElementById('numbers');
+// var checkSymbols = document.getElementById('symbols');
+// var checkLower = document.getElementById('lower')
 var genBtn = document.getElementById('genBtn');
 var pwView = document.getElementById('pwView');
+var checkUpper = "";
+var checkNumbers = "";
+var checkSymbols = "";
+var checkLower = "";
+
 
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -13,16 +20,37 @@ var charTot = "";
 
 genBtn.addEventListener('click', event =>{
         event.preventDefault();
-        charTot = lower;
-        if (checkNumbers.checked){
-            charTot = charTot += numbers;
-        }
-        if (checkSymbols.checked){
-            charTot = charTot += symbols;
-        }
-        if (checkUpper.checked) {
-            charTot = charTot += upper;
-        }
+        charTot = "";
+        checkLower = prompt("Include lower case?(y/n)").charAt(0);
+            if (checkLower == "y"){
+                charTot = charTot += lower;
+            }
+        checkUpper = prompt("Include upper case?(y/n)").charAt(0);
+            if (checkUpper == 'y'){
+                charTot = charTot += upper;
+            }
+        checkNumbers = prompt("Include numbers?(y/n)").charAt(0);
+            if (checkNumbers == 'y'){
+                charTot = charTot += numbers;
+            }
+        checkSymbols = prompt("Include symbols?(y/n)").charAt(0);
+            if (checkSymbols == 'y'){
+                charTot = charTot += symbols;
+            }
+        // this code is left as prompts annoy me as a user and I want to change this back after I turn in
+
+        // if (checkLower.checked){
+        //     charTot = charTot += lower;
+        // }
+        // if (checkNumbers.checked){
+        //     charTot = charTot += numbers;
+        // }
+        // if (checkSymbols.checked){
+        //     charTot = charTot += symbols;
+        // }
+        // if (checkUpper.checked) {
+        //     charTot = charTot += upper;
+        // }
 
         var pwLength = document.getElementById('pwLength').value;
         var pw = genPW(pwLength, charTot);
@@ -37,28 +65,4 @@ var genPW = function(length, charTot) {
     }
     return pw;
 }
-// console.log(lower);
-
-// checkUpper.addEventListener('change', () => {
-//     if (checkUpper.checked){
-//         charTot = lower += upper;
-//         console.log(lower);
-
-//     } else {
-//         charTot = lower;
-//         console.log(lower);
-
-//     }
-// });
-// console.log(lower);
-
-// tstBox.addEventListener('change', () => {
-// if (tstBox.checked) {
-//     console.log('test is checked');
-// }else {
-//     console.log('test is unchecked');
-// }
-
-// });
-
 
